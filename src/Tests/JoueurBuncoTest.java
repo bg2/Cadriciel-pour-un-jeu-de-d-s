aeb95ef02e835cd3e9b2f0ad6518dcae9274bc01
Nam Vu Khanh <phuoc-nam.vu-khanh.1@ens.etsmtl.ca>
2017-03-12 03:17:50 -0400

    Ajout des methodes dans JoueurBuncoTest

--- src/Tests/JoueurBuncoTest.java
+++ src/Tests/JoueurBuncoTest.java
@@ -1,15 +1,59 @@
 package Tests;
 
-import Framework.Joueur;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import Framework.Joueur;
 
 public class JoueurBuncoTest {
 
+	private Joueur joueur1;
+	private Joueur joueur2;
+	private String nom1 = "joueur1";
+	private String nom2 = "joueur2";
+	private int scoreJoueur1 = 21;
+	private int scoreJoueur2 = 5;
+
+	@Before
+	public void initialiser() {
+		joueur1 = new Joueur(nom1);
+		joueur2 = new Joueur(nom2);
+
+	}
+
 	@Test
-	public void test() {
-		
+	public void getNomJoueurTest() {
+		assertEquals(joueur1.getNom(), nom1);
+		assertEquals(joueur2.getNom(), nom2);
+	}
+
+	@Test
+	public void getScoreJoueurTest() {
+		joueur1.incrementerScore(scoreJoueur1);
+		joueur2.incrementerScore(scoreJoueur2);
+		assertTrue(joueur1.getScore() == scoreJoueur1);
+		assertTrue(joueur2.getScore() == scoreJoueur2);
+	}
+
+	@Test
+	public void comparerScoreSuperieurTest() {
+		System.out.println("joueur1 : " + joueur1.getScore());
+		System.out.println("joueur2 : " + joueur2.getScore());
+		System.out.println(joueur1.compareTo(joueur2));
+		assertTrue(joueur1.compareTo(joueur2) == 1);
+	}
+
+	@Test
+	public void comparerScoreInferieurTest() {
+		assertTrue(joueur1.compareTo(joueur2) == -1);
+	}
+
+	@Test
+	public void comparerScoreEquivalentTest() {
+		assertTrue(joueur1.compareTo(joueur2) == 0);
 	}
 
 }
